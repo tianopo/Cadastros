703185306b73acd0839167eb75431bb4ffd366a7
matheus <matheuslink18@hotmail.com>
2023-05-31 09:41:18 -0300

    Construção de useDebounce

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -3,36 +3,42 @@ import { useSearchParams } from 'react-router-dom';
 import { FerramentasDaListagem } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 import { useEffect, useMemo } from 'react';
+import { useDeBounce } from '../../shared/hooks';
 
 
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { debounce } = useDeBounce(1000);
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
   useEffect(() => {
-    
-    PessoasService.getAll(1, busca)
-      .then((result) => {
-        if(result instanceof Error) {
-          alert(result.message);
-        }else{
-          console.log(result);
-        }
-      });
+
+    debounce(() => {
+      PessoasService.getAll(1, busca)
+        .then((result) => {
+          if (result instanceof Error) {
+            alert(result.message);
+          } else {
+            console.log(result);
+          }
+        });
+    });
+
+
   }, [busca]);
 
   return (
-    <LayoutBaseDePagina 
+    <LayoutBaseDePagina
       titulo='Listagem de pessoas'
       barraDeFerramentas={
         <FerramentasDaListagem
           mostrarInputBusca
           textoDaBusca={busca}
           textoBotaoNovo="Novo"
-          aoMudarTextoDeBusca={texto => setSearchParams({ busca: texto}, { replace: true })}
+          aoMudarTextoDeBusca={texto => setSearchParams({ busca: texto }, { replace: true })}
         />
       }>
 
